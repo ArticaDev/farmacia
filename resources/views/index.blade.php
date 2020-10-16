6e4e525c88c2ce0d655dca04954b9ec613419202
Lucas Lima <limalucas454@gmail.com>
2020-10-16 11:37:15 -0300

    Adding initial footer and css/img files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,3 +2,57 @@
 @section('content')
     <p>Content</p>
 @endsection
+@section('footer')
+<div style="font-family: Oswald, sans-serif;font-weight: normal;">
+        <div class="row">
+            <div class="col d-flex d-xl-flex justify-content-center justify-content-xl-center" style="background-color: #ffffff;"><img src="{{asset('assets/img/tim-removebg-preview.png')}}" style="width: 120px;"></div>
+        </div>
+        <!-- Start: Footer Dark -->
+        <footer class="page-footer dark" style="background-color: rgb(203,36,48);">
+            <div class="container" style="margin-top: -30px;">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h5 style="font-weight: normal;">Institucional</h5>
+                        <ul>
+                            <li><a href="#">Trabalhe conosco</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3">
+                        <h5>Serviços</h5>
+                        <ul>
+                            <li><a href="#">Aferir pressão</a></li>
+                            <li><a href="#">Aplicação de vacina</a></li>
+                            <li><a href="#">Controle de peso</a></li>
+                            <li><a href="#">Testes Covid-19</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3">
+                        <h5>Descontos</h5>
+                        <ul>
+                            <li><a href="#">Convênios</a></li>
+                            <li><a href="#">Promoções do dia</a></li>
+                            <li><a href="#">Programa de benefícios</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3">
+                        <h5>Atendimento</h5>
+                        <ul>
+                            <li><a href="#">Como Comprar</a></li>
+                            <li><a href="#">Frete</a></li>
+                            <li><a href="#">Entregas no mesmo dia</a></li>
+                            <li><a href="#">Cancelamentos</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                    <div class="col d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center"><i class="fa fa-whatsapp" style="font-size: 30px;color: rgb(232,232,232);"></i><i class="fa fa-instagram" style="font-size: 30px;margin-left: 5%;color: rgb(232,232,232);"></i><i class="fa fa-linkedin" style="font-size: 30px;margin-left: 5%;color: rgb(232,232,232);"></i></div>
+                </div>
+            </div>
+            <div class="footer-copyright" style="background-color: #671218;border-style: none;margin-top: 30px;">
+                <p style="color: rgb(255,255,255);">Desenvolvido por Artica Dev -&nbsp; 2020&nbsp;</p>
+            </div>
+        </footer>
+        <!-- End: Footer Dark -->
+    </div>
+    <!-- End: Footer -->
+@endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,11 +9,16 @@
     
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Farmacia</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link  href="{{ asset('assets/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/smoothproducts.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/Simple-Slider.css') }}" rel="stylesheet">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
@@ -28,11 +33,12 @@
 </head>
 <body>
 
-<!-- END HEADER --> 
+<!-- END HEADER -->     
 <!-- START CONTENT -->
 @yield('content')
 <!-- END CONTENT -->
 <!-- START FOOTER-->
+@yield('footer')
 
 </body>
 </html>
