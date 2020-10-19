0590c5cd32e0e2642abfcaf6ca87f94dcf51ba47
Lucas Lima <limalucas454@gmail.com>
2020-10-19 16:51:01 -0300

    Adding Slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,23 @@
 @section('content')
     <p>Content</p>
 @endsection
+
+@section('slider')
+<div class="simple-slider">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" style="background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;);"></div>
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+@endsection
+
+
 @section('footer')
 <div style="font-family: Oswald, sans-serif;font-weight: normal;">
         <div class="row">
